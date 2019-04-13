1d79f86ea91c686efac82aa9dd0eabf23774dcdb
dongdong <153399087@qq.com>
2019-04-13 22:15:56 +0800

    pycharmCommit

--- Public/operationExcel.py
+++ Public/operationExcel.py
@@ -7,6 +7,9 @@ class OperationExcel:
         db = xlrd.open_workbook(data_dir('data','data.xlsx'))
         sheet = db.sheet_by_index(0)
         return sheet
+    def get_rows(self):
+        '''获取Excel的行数'''
+        return self.getExcel().nrows
 
 operation = OperationExcel()
-print(operation.getExcel())
+print(operation.get_rows())
